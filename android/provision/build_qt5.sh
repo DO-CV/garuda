31d7ff3ea3580a6e17babb364ff480b1247a3ca6
David OK <david.ok8@gmail.com>
2020-05-03 13:04:28 -0400

    MAINT: fix script.

--- android/provision/build_qt5.sh
+++ android/provision/build_qt5.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -eux
 
 QT_VERSION=v5.14.2
 
-ANDROID_DEVICE_ABI=$(adb shell getprop ro.product.abi)
+ANDROID_DEVICE_ABI=$(adb shell getprop ro.product.cpu.abi)
 ANDROID_SDK_ROOT=${HOME}/Android/Sdk
 ANDROID_NDK_ROOT=${HOME}/opt/android-ndk-r21b
 
@@ -10,11 +11,11 @@ INSTALL_DIR=${HOME}/opt/android-armeabi-v7a
 
 if [ ! -d qt5 ]; then
   git clone git://code.qt.io/qt/qt5.git qt5
+  perl init-repository
 fi
 
 pushd qt5
 {
-  perl init-repository
   git checkout ${QT_VERSION}
   git submodule update --recursive
 
@@ -30,10 +31,10 @@ pushd qt5
     -android-sdk ${ANDROID_SDK_ROOT} \
     -android-abis ${ANDROID_DEVICE_ABI} \
     -no-warnings-are-errors \
-    --prefix ${INSTALL_DIR}
+    -opensource -confirm-license \
+    -prefix ${INSTALL_DIR}
 
   make -j$(nproc)
-
   make install
 }
 popd
